67d1c6caf1941b603e4dd54ddf3c0181038987c3
Taylor Noel <taylor.noel1@outlook.com>
2022-11-28 14:10:50 -0700

    add flight table and form in the admin page

--- flight-system/src/pages/LoginPage.tsx
+++ flight-system/src/pages/LoginPage.tsx
@@ -1,15 +1,51 @@
 import './LoginPage.css';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function LoginPage() {
+    const [authorize, setAuthorize] = useState(false);
+    const [email, setEmail] = useState({ value: '' });
+    const [password, setPassword] = useState({ value: '' });
+
+    const navigate = useNavigate();
+
+    function handleEmail(event: any) {
+        setEmail({ value: event.target.value });
+    }
+    function handlePassword(event: any) {
+        setPassword({ value: event.target.value });
+    }
+    function handleLogin(event: any) {
+        //TODO: authorize credentials
+        if (email.value === "taylor") {
+            navigate('/admin');
+
+        }
+        else {
+            setAuthorize(true);
+        }
+
+    }
 
     return <div className="loginPage">
         <form className="loginForm">
             <label className="title">Login</label>
             <label>Email Address</label>
-            <input type="email" placeholder="Enter email" />
+            <input
+                type="email"
+                value={email.value}
+                placeholder="Enter email"
+                onChange={handleEmail}
+            />
             <label>Password</label>
-            <input type="password" placeholder="Enter password" />
-            <button>Submit</button>
+            <input
+                type="password"
+                value={password.value}
+                placeholder="Enter password"
+                onChange={handlePassword}
+            />
+            <button onClick={handleLogin}>Submit</button>
+            {authorize ? <label>Error: incorrect credentials, please check your email and password.</label> : null}
         </form>
     </div>
 }
