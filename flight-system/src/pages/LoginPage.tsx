256b1ef1f17f79a2e7f16a09f5f5d97d64efb06b
Taylor Noel <taylor.noel1@outlook.com>
2022-11-28 12:24:32 -0700

    initial login page layout

--- flight-system/src/pages/LoginPage.tsx
+++ flight-system/src/pages/LoginPage.tsx
@@ -1,6 +1,16 @@
-function LoginPage () {
+import './LoginPage.css';
 
-    return <div className="landing">
+function LoginPage() {
+
+    return <div className="loginPage">
+        <form className="loginForm">
+            <label className="title">Login</label>
+            <label>Email Address</label>
+            <input type="email" placeholder="Enter email" />
+            <label>Password</label>
+            <input type="password" placeholder="Enter password" />
+            <button>Submit</button>
+        </form>
     </div>
 }
 export default LoginPage;
