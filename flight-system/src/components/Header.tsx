f299c5c5162d101f2b4595014f9121fff97f87b1
Taylor Noel <taylor.noel1@outlook.com>
2022-11-27 23:16:16 -0700

    add navigation to landingpage

--- flight-system/src/components/Header.tsx
+++ flight-system/src/components/Header.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './Header.css';
+import { useNavigate } from 'react-router-dom';
 
+function Header() {
+    const navigate = useNavigate();
 
-const Header = () =>(
-    <div>
-        <label>Alberta Air</label>
-        <button>Sign In</button>
-    </div>
-);
+    function handleLogin(){
+        navigate('/login');
+    }
+    return (<div className="header">
+        <label className="company">Alberta Air</label>
+        <button className="login" onClick={handleLogin}>Login</button>
+    </div>)
+}
 
 export default Header;
--- flight-system/src/components/SearchForm.tsx
+++ flight-system/src/components/SearchForm.tsx
@@ -1,31 +1,38 @@
 import React from "react";
 import './SearchForm.css'
+import { useNavigate } from 'react-router-dom';
+import Select from 'react-select';
 
-function SearchForm (props: any) {
+function SearchForm(props: any) {
+    const list = ["calgary", "victoria", "toronto"];
+
+    const navigate = useNavigate();
+    const handleSearch = (event: { preventDefault: () => void; }) => {
+        navigate('/flights');
+
+    }
     return <div >
-    <form className = "search">
-        <label>
-            From
-            <input type="text" name="from" />
-        </label>
-        <label>
-            To
-            <input type="text" name="to" />
-        </label>
-        <label>
-            Departure Date
-            <input type="date" name="departure" />
-        </label>
-        <label>
-            Return Date
-            <input type="date" name="return" />
-        </label>
-        <label>
-            Passenger(s)
-            <input type="range" name="vol" min="0" max="50" />
-        </label>
-        <input type="submit" value="Search Flights!"/>
-    </form>
+        <form className="search">
+            <label>
+                <Select placeholder="Where from?" options={list.map(t => ({ value: t, label: t }))} />
+            </label>
+            <label>
+                <Select placeholder="Where to?" options={list.map(t => ({ value: t, label: t }))} />
+            </label>
+            <label>
+                Departure Date
+                <input type="date" name="departure" />
+            </label>
+            <label>
+                Return Date
+                <input type="date" name="return" />
+            </label>
+            <label>
+                Passenger(s)
+                <input type="number" name="vol" min="0" max="50" />
+            </label>
+            <input type="button" value="Search Flights!" onClick={handleSearch} />
+        </form>
     </div>
 
 }
