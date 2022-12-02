b95a246f67868316d9f10f2e8fd83987b1c16d5d
Taylor Noel <taylor.noel1@outlook.com>
2022-12-02 11:19:30 -0700

    update styling

--- flight-system/src/components/SearchForm.tsx
+++ flight-system/src/components/SearchForm.tsx
@@ -12,27 +12,35 @@ function SearchForm(props: any) {
 
     }
     return <div >
+        <div className="outline">
+            <div className="covidBlurb outline"><label>Get up to date on the latest COVID-19 measures for your destination.</label> <a href="https://travel.gc.ca/travel-covid">Review Requirements</a></div>
         <form className="search">
-            <label>
-                <Select placeholder="Where from?" options={list.map(t => ({ value: t, label: t }))} />
-            </label>
-            <label>
-                <Select placeholder="Where to?" options={list.map(t => ({ value: t, label: t }))} />
-            </label>
-            <label>
-                Departure Date
+            <Select
+                className="selectInput"
+                placeholder="From"
+                options={list.map(t => ({ value: t, label: t }))} />
+            <Select
+                className="selectInput"
+                placeholder="To"
+                options={list.map(t => ({ value: t, label: t }))} />
+                <div className="space"/>
+            <div className="dateInput">
+                <label>Depart </label>
                 <input type="date" name="departure" />
-            </label>
-            <label>
-                Return Date
+            </div>
+            <div className="dateInput">
+                <label>Return  </label>
                 <input type="date" name="return" />
-            </label>
-            <label>
-                Passenger(s)
-                <input type="number" name="vol" min="0" max="50" />
-            </label>
-            <input type="button" value="Search Flights!" onClick={handleSearch} />
+            </div>
+            
         </form>
+        
+        <input 
+        type="button" 
+        value="Search Flights!" 
+        className="searchButton"
+        onClick={handleSearch} />
+        </div>
     </div>
 
 }
