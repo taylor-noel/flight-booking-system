8794a24e7f59b3677a5927bb3b76f35ca8866ea8
Taylor Noel <taylor.noel1@outlook.com>
2022-12-06 06:09:35 -0700

    add axios requests for search flights

--- flight-system/src/components/SearchForm.tsx
+++ flight-system/src/components/SearchForm.tsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './SearchForm.css'
 import { useNavigate } from 'react-router-dom';
 import Select from 'react-select';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlane } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 function SearchForm(props: any) {
-    const list = ["calgary", "victoria", "toronto"];
+    const [airports, setAirports] = useState<string[]>([]);
+    const [info, setInfo] = useState({
+        departure: '',
+        arrival: '',
+        date: ''
+    });
+
+
+    useEffect(() =>{
+        axios.get('http://127.0.0.1:8000/getAirportCities')
+        .then(response => {
+            const cities: string[] = []
+            response.data.forEach((x: { [x: string]: any; }) =>{cities.push(x["city"])})
+            setAirports(cities);
+        });
+    }, []);
+
+    function handleChange(event:any){
+        const newFormData = { ...info };
+        newFormData[event.target.name as keyof typeof info] = event.target.value;
+
+        setInfo(newFormData);
+    }
+
+    function handleOptionsChange(options: any) {
+        const newFormData = { ...info };
+        newFormData[options.value as keyof typeof info] = options.label;
+
+        setInfo(newFormData);
+    }
 
     const navigate = useNavigate();
     const handleSearch = (event: { preventDefault: () => void; }) => {
-        navigate('/flights');
-
+        navigate('/flights', {state: {departure: info.departure, arrival: info.arrival, date: info.date}});
     }
     return <div >
         <div className="outline">
@@ -20,16 +49,22 @@ function SearchForm(props: any) {
             <Select
                 className="selectInput"
                 placeholder="From"
-                options={list.map(t => ({ value: t, label: t }))} />
+                name="departure"
+                onChange={handleOptionsChange}
+                options={airports.map(t => ({ value: "departure", label: t }))} />
                 <FontAwesomeIcon icon={faPlane} className="planeIcon"/>
             <Select
                 className="selectInput"
                 placeholder="To"
-                options={list.map(t => ({ value: t, label: t }))} />
+                name="arrival"
+                onChange={handleOptionsChange}
+                options={airports.map(t => ({ value: "arrival", label: t }))} />
                 <div className="space"/>
             <div className="dateInput">
                 <label>Depart </label>
-                <input type="date" name="departure" />
+                <input type="date" 
+                 name="date"
+                 onChange={handleChange}/>
             </div>            
         </form>
         
