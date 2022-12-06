c89adb0be1e3e327f09e3025ad739f2f9b835004
unknown <ayoolabode22@gmail.com>
2022-12-06 05:18:10 -0700

    sql

--- backend/index.py
+++ backend/index.py
@@ -8,9 +8,20 @@ from routes.index import flightsbooked
 from routes.index import boardingpasses
 from routes.index import airports
 from routes.index import seats
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(customers)
 app.include_router(admins)
